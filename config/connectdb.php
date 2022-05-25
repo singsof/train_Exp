7dc5ad6002d64c2f3844b936fdf761649759e95c
SING SOFT <singsoft.sw@gmail.com>
2022-05-25 14:22:49 +0700

    connect

---
+++ config/connectdb.php
@@ -0,0 +1,38 @@
+<?php
+
+// require_once("./config.inc.php");
+define("DB_TYPE", "MySQL"); // MySQL & SQLite
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "DB_Name");
+
+define("DB_DNS_MYSQL", "mysql:host=" . DB_HOST . "; dbname=" . DB_NAME);
+define("DB_DNS_SQLITE", "sqlite:db/sqlite_file");
+
+class Database
+{
+    private static $link = null;
+    private static function getLink()
+    {
+        if (self::$link) {
+            return self::$link;
+        }
+        self::$link = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USERNAME, DB_PASSWORD);
+        self::$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return self::$link;
+    }
+
+    public static function __callStatic($name, $args)
+    {
+        $callback = array(self::getLink(), $name);
+        return call_user_func_array($callback, $args);
+    }
+
+
+
+    public static function Con_delete()
+    {
+        self::getLink() == null;
+    }
+}
